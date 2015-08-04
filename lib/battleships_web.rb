e5280d2cf3060f4800bf92fdf74a108115dcacc9
Edwardyao123 <zhigang_edward@hotmail.com>
2015-08-04 10:15:01 +0100

    add new game page

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -1,10 +1,15 @@
 require 'sinatra/base'
 
 class BattleshipsWeb < Sinatra::Base
-  get '/' do
-    'Hello BattleshipsWeb!'
-  end
+  set :views, proc { File.join(root, '..', 'views')}
 
+  get '/' do
+    erb :index
+  end
+  get '/newgame' do
+  	erb :newgame
+  end
+	
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
